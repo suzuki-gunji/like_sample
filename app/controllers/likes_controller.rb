92257ecc8cbc0a151672f50a2e6859879e28935a
suzuki-gunji <gunji1102@gmail.com>
2021-03-11 15:00:01 +0900

    sowコントローラーに@like変数の記述

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -7,6 +7,6 @@ class LikesController < ApplicationController
   def destroy
     @like = Like.find_by(post_id: params[:post_id], user_id: current_user.id)
     @like.destroy
-    redirect_back(fallback_location: root_path)
+    redirect to root_path
   end
 end
