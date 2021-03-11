416eefd4999ca3a0b39201ead4e8936b1bd5c7fa
suzuki-gunji <gunji1102@gmail.com>
2021-03-11 14:30:45 +0900

    postコントローラーの記述

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,25 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
+    @post = Post.new
   end
 
   def show
+    @post = Post.find(params[:id])
   end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_to root_path
+    else  
+      redirect_to root_path
+    end  
+  end
+  
+  private
+  def post_params
+    params.require(:post).permit(:content)
+  end  
 end
