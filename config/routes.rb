d1e220073390bee66bf2dc0e051ad0f69fdf8ee5
suzuki-gunji <gunji1102@gmail.com>
2021-03-11 14:22:31 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/show'
-  get 'users/index'
-  get 'users/show'
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [:index, :show]
+  resources :posts, only: [:index, :show, :create]
+
+  root 'posts#index'
 end
