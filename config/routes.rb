86fe654ce6e8d3d9a57800996b65a5a9400a9bdb
suzuki-gunji <gunji1102@gmail.com>
2021-03-11 14:54:24 +0900

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users, only: [:index, :show]
-  resources :posts, only: [:index, :show, :create]
+  resources :posts, only: [:index, :show, :create] do
+    resources :likes, only: [:create, :destroy]
+  end
 
   root 'posts#index'
 end
